6be7cff20fff4697e7e57ada4799537301d10e87
rayokeeffe <rayokeeffe@gmail.com>
2022-10-23 21:18:20 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    Arm = 1
+})
+input.onButtonPressed(Button.B, function () {
+    Arm = 0
+})
+let Arm = 0
+radio.setGroup(1)
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # # #
+    `)
+Arm = 0
+basic.forever(function () {
+    if (Arm == 1) {
+        led.toggle(2, 2)
+        if (input.rotation(Rotation.Pitch) < 50 && input.rotation(Rotation.Pitch) > -50) {
+            radio.sendValue("left", Math.round(Math.map(Math.constrain(input.rotation(Rotation.Roll), -90, 90), 0, 90, 90, 120)))
+        }
+    }
+})
